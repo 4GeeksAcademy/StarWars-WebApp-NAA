86fd791fc9577e606375a8b0da5f1aeeccdc08ed
Nicksteel91 <107808338+Nicksteel91@users.noreply.github.com>
2025-05-12 22:12:43 +0000

    en proceso

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,40 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import { useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
+	const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
+
+	function apiPersonajes() {
+
+		fetch("https://www.swapi.tech/api/people/")
+			.then(res => res.json())
+			.then((data) => {
+				dispatch({
+					type: "set_personajes:",
+					payload: { personaje: data.results }
+				})
+			})
+				
+			.catch(err => console.error(err));
+	}
+   
+	useEffect(() => {
+		apiPersonajes()
+	}, [])
+	
+
+	// useEffect(() => {
+	// 	fetch("https://www.swapi.tech/api/planets/")
+	// 		.then(res => res.json())
+	// 		.then(data => console.log(data.results))
+	// 		.catch(err => console.error(err));
+	// }, []);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container">
+			<h1>Personajes!!</h1>
 		</div>
+
 	);
-}; 
+};
--- src/store.js
+++ src/store.js
@@ -12,21 +12,35 @@ export const initialStore=()=>{
         title: "Do my homework",
         background: null,
       }
-    ]
+    ],
+    // saludo:"",
+    character:[],
+    // vehiculos:[]
   }
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
 
-      const { id,  color } = action.payload
-
+  switch (action.type) {
+    case 'add_task': 
+      const { id, color } = action.payload;
       return {
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        todos: store.todos.map((todo) => (
+          todo.id === id ? { ...todo, background: color } : todo
+        ))
       };
+
+
+    case "set_personajes": 
+      const { personaje } = action.payload
+      return {
+        ...store,
+        characters: personaje,
+      }
+
     default:
-      throw Error('Unknown action.');
-  }    
+
+      throw new Error('Unknown action.');
+  }
 }
